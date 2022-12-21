4b83c99943b8b02a68e8a01ab278b2487089cdc8
Boi <119715263+cpp-johnny@users.noreply.github.com>
2022-12-21 11:07:18 +0800

    reformat

--- rain-in-chefland.cpp
+++ rain-in-chefland.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-	int T;
-	cin >> T;
-	while(T--){
-	    int n;
-	    cin >> n;
-	    if(n < 3)
-	    cout << "LIGHT" << endl;
-	    else if(n >= 3, n < 7)
-	    cout << "MODERATE" << endl;
-	    else
-	    cout << "HEAVY" << endl;
-	    
-	}
-	return 0;
+  int T;
+  cin >> T;
+  while (T--) {
+    int n;
+    cin >> n;
+    if (n < 3)
+      cout << "LIGHT" << endl;
+    else if (n >= 3, n < 7)
+      cout << "MODERATE" << endl;
+    else
+      cout << "HEAVY" << endl;
+
+  }
+  return 0;
 }
