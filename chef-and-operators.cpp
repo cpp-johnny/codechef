d672ef22759cff9b115f09961a61651bdc8eabac
Johnny <119715263+cpp-johnny@users.noreply.github.com>
2022-12-25 11:36:49 +0800

    Create chef-and-operators.cpp

---
+++ chef-and-operators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+  int t;
+  std::cin >> t;
+
+  while (t--) {
+    int a, b;
+    std::cin >> a >> b;
+
+    if (a < b) {
+      std::cout << '<' << std::endl;
+    } else if (a > b) {
+      std::cout << '>' << std::endl;
+    } else {
+      std::cout << '=' << std::endl;
+    }
+  }
+
+  return 0;
+}
